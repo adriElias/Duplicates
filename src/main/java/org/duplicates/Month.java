bfd4a78fbb35890541de7d859868bbb1d9157f90
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-19 10:53:20 +0100

    feat: Create Month class

---
+++ src/main/java/org/duplicates/Month.java
@@ -0,0 +1,22 @@
+package org.duplicates;
+
+public class Month {
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
