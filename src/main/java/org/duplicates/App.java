7ccd152f86133b5cfd2cfa3d1b232fd33ce93b52
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-19 12:01:29 +0100

    feat: convert to HashSet and verify no duplicates, and add iteration with for-each and Iterator

--- src/main/java/org/duplicates/App.java
+++ src/main/java/org/duplicates/App.java
@@ -1,6 +1,9 @@
 package org.duplicates;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
@@ -27,5 +30,38 @@ public class App {
 
         System.out.println(months);
 
+        System.out.println("\n --- Months with HashSet --- \n");
+
+        Set<Month> monthSet = new HashSet<>(months);
+        System.out.println(monthSet);
+
+        boolean added = monthSet.add(new Month("August"));
+        System.out.println("\nWas it possible to add a duplicate of August?: " + added);
+
+        System.out.println("HashSet size: " + monthSet.size());
+
+        System.out.println("\n -- For-each without MonthSet -- \n");
+        for (Month m : months) {
+            System.out.println(m);
+        }
+
+        System.out.println("\n -- For-each with MonthSet -- \n");
+        for (Month m : monthSet) {
+            System.out.println(m);
+        }
+
+        System.out.println("\n-- Iterator without MonthSet-- \n");
+        Iterator<Month> iterator = months.iterator();
+        while (iterator.hasNext()) {
+            Month m = iterator.next();
+            System.out.println(m);
+        }
+
+        System.out.println("\n-- Iterator with MonthSet-- \n");
+        Iterator<Month> iteratorSet = monthSet.iterator();
+        while (iteratorSet.hasNext()) {
+            Month m = iteratorSet.next();
+            System.out.println(m);
+        }
     }
 }
