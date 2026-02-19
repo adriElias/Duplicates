0030226ec8bebc15d87d9cd123a8282e57286661
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-19 11:02:59 +0100

    feat: Add 11 months without August in ArrayList months

--- src/main/java/org/duplicates/App.java
+++ src/main/java/org/duplicates/App.java
@@ -1,13 +1,26 @@
 package org.duplicates;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+
+        ArrayList<Month> months = new ArrayList<>();
+        System.out.println("--- Months without August--- \n");
+        months.add(new Month("January"));
+        months.add(new Month("February"));
+        months.add(new Month("March"));
+        months.add(new Month("April"));
+        months.add(new Month("May"));
+        months.add(new Month("June"));
+        months.add(new Month("July"));
+        //August
+        months.add(new Month("September"));
+        months.add(new Month("October"));
+        months.add(new Month("November"));
+        months.add(new Month("December"));
+
+        System.out.println(months);
+
     }
 }
